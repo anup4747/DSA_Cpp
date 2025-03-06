6b66f7fafc3cc0477a9abce3fddcb225a554c723
Anup <119606225+anup4747@users.noreply.github.com>
2025-03-06 23:10:59 +0530

    kadane's algorithm

---
+++ Vectors/p7.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// Maximum Subarray
+
+class Solution {
+    public:
+        int maxSubArray(vector<int>& nums) {
+            int currSum = 0, maxSum = INT_MIN;
+    
+            for(int val : nums){
+                currSum += val;
+                maxSum = max(currSum, maxSum);
+                if(currSum < 0 ){
+                    currSum= 0;
+                }
+            }
+            return maxSum;
+        }
+    };
